dad0271651eb46237eec3c131064e75b79032d10
jongsoo1203 <114615490+jongsoo1203@users.noreply.github.com>
2023-10-20 14:25:19 -0400

    Delete project1 directory

--- project1/name_age.py
+++
@@ -1,9 +0,0 @@
-import stdio
-import sys
-
-# Accept name (str) and age (str) as command-line arguments.
-name = sys.argv[1]
-age = sys.argv[2]
-
-# Write the message "name is age years old." to standard output.
-stdio.writeln(str(name) + " is " + str(age) + " years old.")
